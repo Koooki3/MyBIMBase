4a2685c4519b72ba1911ff8517b22198b4cc9212
MyBIMBase <1498306179@qq.com>
2025-01-22 17:37:23 +0800

    MyBIMBase_2025R1.0_补充两点画线工具（BPPrimitiveTool实现），优化HelloBIMBase.cpp

--- MyBIMBase/HelloBIMBase.cpp
+++ MyBIMBase/HelloBIMBase.cpp
@@ -5,6 +5,5 @@ void HelloBIMBase()
 	AfxMessageBox(L"Hello BIMBase! This is writer kooki3.");
 }
 AutoDoRegisterFunctionsBegin
-BIMBase::BPToolsManager::registerFun(_T("KOOKI3"), &HelloBIMBase);
+BIMBase::BPToolsManager::registerFun(_T("kooki3"), &HelloBIMBase);
 AutoDoRegisterFunctionsEnd
-
